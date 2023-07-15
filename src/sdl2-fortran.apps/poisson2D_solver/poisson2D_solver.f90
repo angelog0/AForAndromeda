559f7dd23d577fc1de584b45e9496e6215e45f28
angelog0 <angelo.g0@libero.it>
2023-07-15 15:09:08 +0200

    Miscellaneous adjustments

--- src/sdl2-fortran.apps/poisson2D_solver/poisson2D_solver.f90
+++ src/sdl2-fortran.apps/poisson2D_solver/poisson2D_solver.f90
@@ -2,7 +2,7 @@
 ! Author: ANGELO GRAZIOSI
 !
 !   created   : Dec 21, 2022
-!   last edit : Dec 24, 2022
+!   last edit : Jul 15, 2023
 !
 !   Boundary Value Problem for Poisson Equation
 !
@@ -131,7 +131,7 @@ module poisson2D_solver_lib
        '  2 : GRADIENT ' ]
 
   ! Input data
-  integer :: ndiv = 100, max_iter = 100, nsout = 10, &
+  integer :: ndiv = 500, max_iter = 100, nsout = 10, &
        config_type = ONE_BOX, field_type = POTENTIAL_FIELD
   real(WP) :: eps = 0.0001_WP, omega = 1.8_WP, &
        u_left = ONE, u_right = ZERO, &
@@ -310,180 +310,22 @@ contains
     end if
   end subroutine setup_aux_params
 
-  subroutine setup_grid()
-    real(WP) :: x, y
-    integer :: i, j
-
-    ! Grid initialization
-    ! The method, as you can see, NEVER uses the values of density s(:,:) on
-    ! boundaries! This is used as a trick in computing the energy: we
-    ! set s(:,:) on boundary to ZERO.
-    ! b(i,j) == true if in x = i*h, y = j*h there is a boundary condition
-    !
-    ! First the bottom and top side...
-    do i = 0, ndiv
-       u(i,0) = u_bottom
-       u(i,ndiv) = u_top
-       s(i,0) = ZERO
-       s(i,ndiv) = ZERO
-       f(i,0) = ZERO
-       f(i,ndiv) = ZERO
-       b(i,0) = .true.
-       b(i,ndiv) = .true.
-    end do
-
-    ! ...then the left and right side...
-    do j = 0, ndiv
-       u(0,j) = u_left
-       u(ndiv,j) = u_right
-       s(0,j) = ZERO
-       s(ndiv,j) = ZERO
-       f(0,j) = ZERO
-       f(ndiv,j) = ZERO
-       b(0,j) = .true.
-       b(ndiv,j) = .true.
-    end do
-
-    ! ...then the inner nodes
-    do i = 1, ndiv-1
-       x = i*h
-       do j = 1, ndiv-1
-          y = j*h
-          select case(config_type)
-          case(ONE_BOX)
-             u(i,j) = ZERO
-             s(i,j) = ZERO
-             b(i,j) = .false.
-          case(TWO_BOX)
-             call fcn_two_box(i,j,u(i,j),s(i,j),b(i,j))
-          case(CONDENSER)
-             call fcn_condenser(i,j,u(i,j),s(i,j),b(i,j))
-          case(THREE_CHARGES)
-             call fcn_three_charges(x,y,u(i,j),s(i,j),b(i,j))
-          case(CHARGED_LINE)
-             call fcn_charged_line(x,y,u(i,j),s(i,j),b(i,j))
-
-          case default
-             ! ONE_BOX
-             u(i,j) = ZERO
-             s(i,j) = ZERO
-             b(i,j) = .false.
-          end select
-
-          s(i,j) = s(i,j)*hq
-          f(i,j) = ZERO
-       end do
-    end do
-  end subroutine setup_grid
-
-  function get_grid_energy() result (r)
-    real(WP) :: r
-    !
-    ! Koonin's formula (6.7):
-    !
-    ! E = 0.5*Sum(i=1,N)Sum(j=1,N)[(u(i,j)-u(i-1,j))**2+(u(i,j)-u(i,j-1))**2]
-    !   -h*h*Sum(i=1,N-1)Sum(j=1,N-1)[S(i,j)*u(i,j)]
-    !
-    integer :: i = 0, j = 0
-    real(WP) :: sum1 = ZERO, sum2 = ZERO, ff = ZERO
-
-    sum1 = ZERO
-    sum2 = ZERO
-    ff = ZERO
-
-    do i = 1, ndiv
-       do j = 1, ndiv
-
-          ! The (length of the) gradient
-          ff = hypot(u(i,j)-u(i-1,j),u(i,j)-u(i,j-1))
-
-          f(i,j) = ff
-
-          ! The total sum of the gradient squared
-          sum1 = sum1+ff*ff
-
-          ! On i == ndiv, j == ndiv we have s == 0: this means summing
-          ! for i = 1,N-1, j = 1,N-1
-          sum2 = sum2+s(i,j)*u(i,j)
-       end do
-    end do
-
-    ! Completing the calculus of the field.
-    ! We assume the continuity of the field
-    f(1:ndiv,0) = f(1:ndiv,1)
-    f(0,1:ndiv) = f(1,1:ndiv)
-    f(0,0) = f(1,1)
-
-    ! s(i,j) = hq*S(i*h,j*h)
-    r = HF*sum1-sum2
-  end function get_grid_energy
-
-  subroutine update_grid()
-    integer :: i = 0, j = 0
-
-    do i = 1, ndiv-1
-       do j = 1, ndiv-1
-          if (b(i,j)) cycle
-          u(i,j) = omega1*u(i,j) &
-               +omega4*(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j-1)+s(i,j))
-       end do
-    end do
-  end subroutine update_grid
-
-  subroutine display_grid()
-    use SDL2_shading, only: MAX_COLOUR_INDEX, get_shading_color, color_rgb_t
-    use SDL2_app, only: set_rgba_color, draw_point, refresh
-
-    character(len=*), parameter :: FMT = '(a,i6,2(a,1pg14.7),a,i6,a,1pg14.7)'
-    ! https://zmoon.github.io/FortranTipBrowser/tips/041.html
-    ! character(len=*), parameter :: FMT = "(*(g0.7, :, ','))"
-
-    ! Implicit saving
-    integer :: i = 0, j = 0, k = 0
-    type(color_rgb_t) :: c = color_rgb_t(0,0,0)
-
-    write(*,FMT) 'NDIV = ',ndiv,'   OMEGA = ',omega,'   EPS = ',eps, &
-         '   COUNT = ',count_iter,'   E = ',energy
-
-    do j = 0, ndiv
-       do i = 0, ndiv
-          !
-          ! Remember that PHI(:,:), on the first row, stores the left
-          ! side of the plane while the last row stores the right.  On
-          ! the first column, it stores the bottom and on the last
-          ! column it stores the top of the plane...
-          !
-          ! We scan PHI(:,:) by columns starting from the last (i.e
-          ! from the top of the plane)..
-          !
-          k = int((phi(i,ndiv-j)-phi_min)/dphi)
-
-          ! A safe guard
-          if (k < 0) then
-             k = 0
-          else if (k > MAX_COLOUR_INDEX) then
-             k = MAX_COLOUR_INDEX
-          end if
-
-          c = get_shading_color(k)
-
-          ! .. and plotting by rows
-          call set_rgba_color(c%r,c%g,c%b)
-          call draw_point(i,j)
-       end do
-    end do
-    call refresh()
-  end subroutine display_grid
-
   subroutine solve()
-    ! Switch ON the grid...
-    call grid_on()
+    use SDL2_app, only: quit
 
-    call setup_aux_params()
+    character(len=*), parameter :: FMT = '(*(g0,1x))'
+
+    ! Initialize counter
+    count_iter = 0
+
     call setup_grid()
 
     ! Initialization of energy for convergence flags
     energy = get_grid_energy()
+
+    ! Display initial solution
+    write(*,*)
+    call display_grid()
 
     converg = .false.
     do count_iter = 1, max_iter
@@ -492,19 +334,189 @@ contains
        call update_grid()         ! ...then UPDATE the grid
        energy = get_grid_energy() ! Get the energy for the updated grid
 
-       ! Set convergenze flag
+       ! Set convergence flag
        if (abs(energy-energy_old) <= eps) converg = .true.
 
-       if (mod(count_iter,nsout) == 0 .or. converg) then
-          call display_grid()
-       end if
+       if (mod(count_iter,nsout) == 0 .or. converg) call display_grid()
 
        ! Convergence test
        if (converg) exit
+
+       if (quit()) then
+          write(*,*)
+          write(*,FMT) 'Stopped at the iteration : ', count_iter
+          write(*,*)
+          exit
+       end if
     end do
 
-    ! Switch OFF the grid...
-    call grid_off()
+  contains
+
+    subroutine setup_grid()
+      real(WP) :: x, y
+      integer :: i, j
+
+      ! Grid initialization
+      ! The method, as you can see, NEVER uses the values of density s(:,:) on
+      ! boundaries! This is used as a trick in computing the energy: we
+      ! set s(:,:) on boundary to ZERO.
+      ! b(i,j) == true if in x = i*h, y = j*h there is a boundary condition
+      !
+      ! First the bottom and top side...
+      do i = 0, ndiv
+         u(i,0) = u_bottom
+         u(i,ndiv) = u_top
+         s(i,0) = ZERO
+         s(i,ndiv) = ZERO
+         f(i,0) = ZERO
+         f(i,ndiv) = ZERO
+         b(i,0) = .true.
+         b(i,ndiv) = .true.
+      end do
+
+      ! ...then the left and right side...
+      do j = 0, ndiv
+         u(0,j) = u_left
+         u(ndiv,j) = u_right
+         s(0,j) = ZERO
+         s(ndiv,j) = ZERO
+         f(0,j) = ZERO
+         f(ndiv,j) = ZERO
+         b(0,j) = .true.
+         b(ndiv,j) = .true.
+      end do
+
+      ! ...then the inner nodes
+      do i = 1, ndiv-1
+         x = i*h
+         do j = 1, ndiv-1
+            y = j*h
+            select case(config_type)
+            case(ONE_BOX)
+               u(i,j) = ZERO
+               s(i,j) = ZERO
+               b(i,j) = .false.
+            case(TWO_BOX)
+               call fcn_two_box(i,j,u(i,j),s(i,j),b(i,j))
+            case(CONDENSER)
+               call fcn_condenser(i,j,u(i,j),s(i,j),b(i,j))
+            case(THREE_CHARGES)
+               call fcn_three_charges(x,y,u(i,j),s(i,j),b(i,j))
+            case(CHARGED_LINE)
+               call fcn_charged_line(x,y,u(i,j),s(i,j),b(i,j))
+
+            case default
+               ! ONE_BOX
+               u(i,j) = ZERO
+               s(i,j) = ZERO
+               b(i,j) = .false.
+            end select
+
+            s(i,j) = s(i,j)*hq
+            f(i,j) = ZERO
+         end do
+      end do
+    end subroutine setup_grid
+
+    function get_grid_energy() result (r)
+      real(WP) :: r
+      !
+      ! Koonin's formula (6.7):
+      !
+      ! E = 0.5*Sum(i=1,N)Sum(j=1,N)[(u(i,j)-u(i-1,j))**2+(u(i,j)-u(i,j-1))**2]
+      !   -h*h*Sum(i=1,N-1)Sum(j=1,N-1)[S(i,j)*u(i,j)]
+      !
+      integer :: i = 0, j = 0
+      real(WP) :: sum1 = ZERO, sum2 = ZERO, ff = ZERO
+
+      sum1 = ZERO
+      sum2 = ZERO
+      ff = ZERO
+
+      do i = 1, ndiv
+         do j = 1, ndiv
+
+            ! The (length of the) gradient
+            ff = hypot(u(i,j)-u(i-1,j),u(i,j)-u(i,j-1))
+
+            f(i,j) = ff
+
+            ! The total sum of the gradient squared
+            sum1 = sum1+ff*ff
+
+            ! On i == ndiv, j == ndiv we have s == 0: this means summing
+            ! for i = 1,N-1, j = 1,N-1
+            sum2 = sum2+s(i,j)*u(i,j)
+         end do
+      end do
+
+      ! Completing the calculus of the field.
+      ! We assume the continuity of the field
+      f(1:ndiv,0) = f(1:ndiv,1)
+      f(0,1:ndiv) = f(1,1:ndiv)
+      f(0,0) = f(1,1)
+
+      ! s(i,j) = hq*S(i*h,j*h)
+      r = HF*sum1-sum2
+    end function get_grid_energy
+
+    subroutine display_grid()
+      use SDL2_shading, only: MAX_COLOUR_INDEX, get_shading_color, color_rgb_t
+      use SDL2_app, only: set_rgba_color, draw_point, refresh
+
+      character(len=*), parameter :: FMT = '(a,i6,2(a,1pg14.7),a,i6,a,1pg14.7)'
+      ! https://zmoon.github.io/FortranTipBrowser/tips/041.html
+      ! character(len=*), parameter :: FMT = "(*(g0.7, :, ','))"
+
+      ! Implicit saving
+      integer :: i = 0, j = 0, k = 0
+      type(color_rgb_t) :: c = color_rgb_t(0,0,0)
+
+      write(*,FMT) 'NDIV = ',ndiv,'   OMEGA = ',omega,'   EPS = ',eps, &
+           '   COUNT = ',count_iter,'   E = ',energy
+
+      do j = 0, ndiv
+         do i = 0, ndiv
+            !
+            ! Remember that PHI(:,:), on the first row, stores the left
+            ! side of the plane while the last row stores the right.  On
+            ! the first column, it stores the bottom and on the last
+            ! column it stores the top of the plane...
+            !
+            ! We scan PHI(:,:) by columns starting from the last (i.e
+            ! from the top of the plane)..
+            !
+            k = int((phi(i,ndiv-j)-phi_min)/dphi)
+
+            ! A safe guard
+            if (k < 0) then
+               k = 0
+            else if (k > MAX_COLOUR_INDEX) then
+               k = MAX_COLOUR_INDEX
+            end if
+
+            c = get_shading_color(k)
+
+            ! .. and plotting by rows
+            call set_rgba_color(c%r,c%g,c%b)
+            call draw_point(i,j)
+         end do
+      end do
+      call refresh()
+    end subroutine display_grid
+
+    subroutine update_grid()
+      integer :: i = 0, j = 0
+
+      do i = 1, ndiv-1
+         do j = 1, ndiv-1
+            if (b(i,j)) cycle
+            u(i,j) = omega1*u(i,j) &
+                 +omega4*(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j-1)+s(i,j))
+         end do
+      end do
+    end subroutine update_grid
+
   end subroutine solve
 
   subroutine run()
@@ -513,6 +525,11 @@ contains
          QUIT_EVENT, get_event
 
     integer :: ievent = -1000
+
+    ! Switch ON the grid...
+    call grid_on()
+
+    call setup_aux_params()
 
     call shading_setup()
 
@@ -532,6 +549,8 @@ contains
 
     call close_graphics()
 
+    ! Switch OFF the grid...
+    call grid_off()
   end subroutine run
 
   subroutine show_params()
