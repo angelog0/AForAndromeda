1422faa4ad5f6066da0734e7debd8d03098178b0
angelog0 <angelo.g0@libero.it>
2023-07-15 17:11:07 +0200

    Update build script

--- build-all.sh
+++ build-all.sh
@@ -275,6 +275,23 @@ if [ ! -f "${BIN_DIR}/${program_name}${EXE}" ] ; then
     echo "done."
 fi
 
+cd ../complex-dynamics
+
+program_name="complexplot"
+if [ ! -f "${BIN_DIR}/${program_name}${EXE}" ] ; then
+    echo -n "Building ${program_name^^} ... "
+    rm -rf *.mod
+    ${FC} -std=f2018 -O3 -Wall \
+          ${BMODS_DIR}/{{kind,math}_consts,additional_functions,ft_timer_m,utilities,getdata,nicelabels}.f90 ${SDL2F90} \
+          ../../fparser-fortran/fparser_cd.f90 ../SDL2_app.f90 \
+          ${program_name}.f90 ${LIBS} \
+          -L ../../fparser-fortran -lFParser -lstdc++ \
+          -o ${program_name}${EXE}
+    rm -rf *.mod
+    mv ${program_name}${EXE} "${BIN_DIR}"
+    echo "done."
+fi
+
 cd "${NBODY_DIR}"
 
 program_name="calc_orbits"
